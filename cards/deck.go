a1e141399f515a1989ad23ae5db6c1b40e67f3c8
Vikas <vikas.kumar.singh1198@gmail.com>
2025-01-05 18:17:34 +0530

    wrote deck functions

--- cards/deck.go
+++ cards/deck.go
@@ -7,16 +7,23 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardsSuit := []string{"Spade", "Heart", "Club", "Diamond"}
+
+	cardsValue := []string{"Ace", "2", "3", "4"}
+
+	for _, suit := range cardsSuit {
+		for _, value := range cardsValue {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 func (cards deck) print() {
 	for i, card := range cards {
 		fmt.Println(i, card)
-	}
-}
-
-type nums []int
-
-func (n nums) print() {
-	for num := range n {
-		fmt.Println(n[num])
 	}
 }
--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", "Queen of Hearts"}
+	cards := newDeck()
 	cards.print()
-
-	number := nums{1, 2, 3, 4, 5, 6, 7, 8}
-	number.print()
 }
